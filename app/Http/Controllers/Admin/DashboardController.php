7f5e34a5712180a2b72b4aa2293c62a3710650d8
ashokkumar2342 <ashokkumar2342@gmail.com>
2018-09-15 21:05:11 +0530

    attendace show

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Admin;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\ClassType;
+use App\Model\StudentAttendance;
+use App\Student;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -19,7 +24,14 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('admin/dashboard');
+         $today = Carbon::today();
+
+        $admin = Auth::guard('admin')->user()->id;
+        $totalStudents = Student::where('admin_id',$admin)->count();
+        $present = StudentAttendance::where('admin_id',$admin)->where('attendance_type_id',1)->whereDate('created_at',$today)->count();
+        $absent = StudentAttendance::where('admin_id',$admin)->where('attendance_type_id',2)->whereDate('created_at',$today)->count();
+        $class = ClassType::where('admin_id',$admin)->count();
+        return view('admin/dashboard',compact('totalStudents','present','absent','class'));
         
     }
 
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -20,25 +20,23 @@
        <div class="row">
          <div class="col-lg-3 col-xs-6">
            <!-- small box -->
-           <div class="small-box bg-aqua">
+           <div class="small-box bg-yellow">
              <div class="inner">
-               <h3>50</h3> 
-               <p>New Orders</p>
+               <h3>{{ $totalStudents }}</h3> 
+               <p>Students</p>
              </div>
              <div class="icon">
-               <i class="ion ion-bag"></i>
+               <i class="ion ion-person-add"></i>
              </div>
              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
            </div>
          </div>
-         <!-- ./col -->
          <div class="col-lg-3 col-xs-6">
            <!-- small box -->
-           <div class="small-box bg-green">
+           <div class="small-box bg-aqua">
              <div class="inner">
-               <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-               <p>Bounce Rate</p>
+               <h3>{{ $present }}</h3> 
+               <p>Present</p>
              </div>
              <div class="icon">
                <i class="ion ion-stats-bars"></i>
@@ -49,28 +47,31 @@
          <!-- ./col -->
          <div class="col-lg-3 col-xs-6">
            <!-- small box -->
-           <div class="small-box bg-yellow">
+           <div class="small-box bg-green">
              <div class="inner">
-               <h3>500</h3> 
-               <p>Students</p>
+               <h3>{{ $absent }}</h3>
+
+               <p>Absent</p>
              </div>
              <div class="icon">
-               <i class="ion ion-person-add"></i>
+               <i class="ion ion-stats-bars"></i>
              </div>
              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
            </div>
          </div>
          <!-- ./col -->
+        
+         <!-- ./col -->
          <div class="col-lg-3 col-xs-6">
            <!-- small box -->
            <div class="small-box bg-red">
              <div class="inner">
-               <h3>65</h3>
+               <h3>{{ $class }}</h3>
 
-               <p>Unique Visitors</p>
+               <p>Class</p>
              </div>
              <div class="icon">
-               <i class="ion ion-pie-graph"></i>
+               <i class="ion ion-home"></i>
              </div>
              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
            </div>
